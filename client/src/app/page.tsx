25869838ce4ea49e3006508b863665d191dc5b9c
Merayven <mpopiel@tuta.io>
2024-12-29 02:40:27 +0100

    Created & imported dashboard page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,5 @@
+import Dashboard from "@/app/dashboard/page";
+
 export default function Home() {
-  return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      hello
-    </main>
-  );
+  return <Dashboard />;
 }
