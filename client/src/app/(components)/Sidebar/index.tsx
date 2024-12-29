3a6e62837a4bf9df470f12979600fa6fba2ca8f6
Merayven <mpopiel@tuta.io>
2024-12-29 05:08:27 +0100

    Designed props for Sidebar Links

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -2,8 +2,38 @@
 
 import { useAppDispatch, useAppSelector } from '@/app/redux';
 import { setIsSidebarCollapsed } from '@/state';
-import { Menu } from 'lucide-react'
+import { Icon, LucideIcon, Menu } from 'lucide-react'
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react'
+
+interface SidebarLinkProps {
+    href: string;
+    icon: LucideIcon;
+    label: string;
+    isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+    href,
+    icon: Icon,
+    label,
+    isCollapsed
+}: SidebarLinkProps) => {
+    const pathname = usePathname();
+    const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+
+    return (
+        <Link href={href}>
+            <div className={`cursor-pointer flex items-center ${isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"} hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${isActive ? "bg-blue-200 text-white" : ""}`}>  
+                <Icon className="w-6 h-6 !text-gray-700" />
+                <span className={`${isCollapsed ? "hidden" : "block"} font-medium text-gray-700`}>
+                    {label}
+                </span>
+            </div>
+        </Link>
+    )
+}
 
 const Sidebar = () => {
 
