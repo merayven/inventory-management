e8580d886e950686dd5a300114fcf98dc33c5bbc
Merayven <mpopiel@tuta.io>
2024-12-29 02:51:11 +0100

    Fixed unclosed tag

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <DashboardWrapper>{children}</DashboardWrapper>
+      </body>
     </html>
   );
 }
