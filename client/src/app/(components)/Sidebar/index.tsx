c0478a9d7eed8608bd8cb1233388508a182850a5
Merayven <mpopiel@tuta.io>
2024-12-29 05:00:41 +0100

    Hiding company's name on sidebar collapse

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
         {/* TOP LOGO */}
         <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
             <div>logo</div>
-            <h1 className="font-extrabold text-2xl">FIRMA</h1>
+            <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-2xl`}>FIRMA</h1>
             <button 
                 className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" 
                 onClick={toggleSidebar}
