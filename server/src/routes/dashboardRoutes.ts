e9b9ba7c0afffb310c2f8b0bf89cbaac32f97ad3
Merayven <mpopiel@tuta.io>
2024-12-31 00:01:53 +0100

    Created route for /dashboard

--- server/src/routes/dashboardRoutes.ts
+++ server/src/routes/dashboardRoutes.ts
@@ -1,1 +1,8 @@
-import { Router } from "express";
+import { Router } from "express"
+import { getDashboardMetrics } from "../controllers/dashboardController"
+
+const router = Router();
+
+router.get("/", getDashboardMetrics);
+
+export default router;
