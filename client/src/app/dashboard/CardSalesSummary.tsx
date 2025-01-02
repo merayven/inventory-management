d50e7efcca0b05a43c6b2e084acc5f3e23a7ea8c
Merayven <mpopiel@tuta.io>
2025-01-02 21:22:26 +0100

    SHowing Sales summary value in dashboard component

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -7,12 +7,37 @@ const CardSalesSummary = () => {
 
     const [timeframe, setTimeframe] = useState("weekly");
 
+    const totalValueSum = saleData.reduce((acc, curr) => acc + curr.totalValue, 0) || 0;
+
     if (isError) {
         return <div className="m-5">Failed to fetch data</div>
     }
 
   return (
-    <div className="row-span-3 xl:row-span-6 bg-gray-500">CardSalesSummary</div>
+    <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl flex flex-col justify-between">
+        {isLoading ? (
+            <div className="m-5">Loading...</div>
+        ): (
+            <>
+                <div>
+                    <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+                        Sales Summary
+                    </h2>
+                </div>
+
+                <div>
+                    <div className="flex justify-between items-center mb-6 px-7">
+                        <div className="text-lg font-medium">
+                            <p className="text-cs text-gray-400">Value</p>
+                        </div>
+                        <span className="text-2xl font-extrabold">
+                            ${(totalValueSum / 1000000).toLocaleString("en", {maximumFractionDigits: 2,})}m
+                        </span>
+                    </div>
+                </div>
+            </>
+        )}
+    </div>
   )
 }
 
