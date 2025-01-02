5854d5c92cb08c8c145a657d3b747ea908df96dd
Merayven <mpopiel@tuta.io>
2025-01-02 21:42:19 +0100

    Setup for chart in salesummary Card

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -1,6 +1,7 @@
 import { useGetDashboardMetricsQuery } from '@/state/api';
 import { TrendingUp } from 'lucide-react';
 import React, { useState } from 'react'
+import { BarChart, CartesianGrid, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 
 const CardSalesSummary = () => {
     const { data, isLoading, isError } = useGetDashboardMetricsQuery();
@@ -42,7 +43,34 @@ const CardSalesSummary = () => {
                                 {averageChangePercentage.toFixed(2)}%
                             </span>
                         </div>
+                        <select 
+                            className="shadow-sm border border-gray-300 bg-white p-2 rounded"
+                            value={timeframe}
+                            onChange={(e) => { setTimeframe(e.target.value); }}
+                        >
+                            <option value="daily">Daily</option>
+                            <option value="weekly">Weekly</option>
+                            <option value="monthly">Monthly</option>
+                        </select>
                     </div>
+                    <ResponsiveContainer width="100%" height={350} className="px-7">
+                        <BarChart
+                            data={saleData}
+                            margin={{ top: 0, right:0, left: -25, bottom:0 }}
+                        >
+                            <CartesianGrid strokeDasharray="" vertical={false} />
+                            <XAxis dataKey="date" tickFormatter={(value) => {
+                                const date = new Date(value);
+                                return `${date.getMonth() + 1}/${date.getDate()}`;
+                            }}
+                            />
+                            <YAxis tickFormatter={(value) => {
+                                const date = new Date(value);
+                                return `${date.getMonth() + 1}/${date.getDate()}`;
+                            }}
+                            />
+                        </BarChart>
+                    </ResponsiveContainer>
                 </div>
             </>
         )}
