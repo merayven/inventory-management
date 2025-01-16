9f77584eee2e2ee07e26e24540b109aa2088a4e1
Merayven <mpopiel@tuta.io>
2025-01-16 16:59:53 +0100

    Initialised purchase summary card

---
+++ client/src/app/dashboard/CardPurschaseSummary.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardPurschaseSummary = () => {
+  return (
+    <div>CardPurschaseSummary</div>
+  )
+}
+
+export default CardPurschaseSummary
--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import CardExpenseSummary from "./CardExpenseSummary";
 import CardPopularProducts from "./CardPopularProducts";
+import CardPurschaseSummary from "./CardPurschaseSummary";
 import CardSalesSummary from "./CardSalesSummary";
 
 const Dashboard = () => {
@@ -9,6 +10,7 @@ const Dashboard = () => {
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
       <CardSalesSummary />
+      <CardPurschaseSummary />
       <CardExpenseSummary />
       <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500"></div>
       <div className="row-span-3 bg-gray-500"></div>
