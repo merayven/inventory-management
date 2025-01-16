0e1747276e2f90fac83f820839d4b7a2ca392b2e
Merayven <mpopiel@tuta.io>
2025-01-16 18:28:17 +0100

    Added labelFormatter

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -82,6 +82,14 @@ const CardSalesSummary = () => {
                             <Tooltip formatter={(value: number) => [
                                 `$${value.toLocaleString("en")}`,
                             ]}
+                            labelFormatter={(label) => {
+                                const date = new Date(label);
+                                return date.toLocaleDateString("en-US", {
+                                    year: "numeric",
+                                    month: "long",
+                                    day: "numeric",
+                                })
+                            }}
                             />
                             <Bar
                                 dataKey="totalValue"
