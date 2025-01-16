6ff78c9356a3ad4666a36ffb47adc5865fcf468e
Merayven <mpopiel@tuta.io>
2025-01-16 20:51:10 +0100

    Stat crad component with props set up

---
+++ client/src/app/dashboard/StatCard.tsx
@@ -0,0 +1,36 @@
+import { LucideIcon } from 'lucide-react';
+import React, { JSX } from 'react'
+
+type StatDetail = {
+    title: string;
+    amount: string;
+    changePercentage: number;
+    IconComponent: LucideIcon;
+}
+
+type StatCardProps = {
+    title: string;
+    primaryIcon: JSX.Element;
+    details: StatDetail[];
+    dateRange: string;
+}
+
+const StatCard = ({
+    title,
+    primaryIcon,
+    details,
+    dateRange,
+}: StatCardProps) => {
+    const formatPercentage = (value: number) => {
+        const signal = value >= 0 ? "+" : "";
+        return `${signal}${value.toFixed()}%`;
+    }
+
+    const getChangeColor = (value: number) => value >= 0? "text-green-500" : "text-red-500";
+
+  return (
+    <div className="md:row-span-1 xl:row-span-2 bg-white col-span-1 shadow-md rounded-2xl flex flex-col justify-between">StatCard</div>
+  )
+}
+
+export default StatCard
