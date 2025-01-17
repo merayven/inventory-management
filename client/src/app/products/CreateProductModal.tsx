bffd6e9a8d31a4352ebf14532858425bd3da9e7b
Merayven <mpopiel@tuta.io>
2025-01-17 13:48:55 +0100

    Added inputs for all of the fields & buttons create/cancel

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -52,7 +52,8 @@ const CreateProductModal = ({ isOpen, onClose, onCreate }: CreateProductModalPro
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
             <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
                 <Header name="Create New Product" />
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit} className="mt-5">
+                    {/* PRODUCT NAME */}
                     <label htmlFor="productName" className={labelCssStyles}>
                         Product Name
                     </label>
@@ -65,6 +66,69 @@ const CreateProductModal = ({ isOpen, onClose, onCreate }: CreateProductModalPro
                         className={inputCssStyles}
                         required 
                     />
+
+                    {/* PRODUCT BRAND */}
+                    <label htmlFor="productBrand" className={labelCssStyles}>
+                        Product Brand
+                    </label>
+                    <input 
+                        type="text" 
+                        name="brand" 
+                        placeholder="Brand" 
+                        onChange={handleChange} 
+                        value={formData.brand} 
+                        className={inputCssStyles}
+                        required 
+                    />
+
+                    {/* PRODUCT SIZE */}
+                    <label htmlFor="productSize" className={labelCssStyles}>
+                        Product Size
+                    </label>
+                    <input 
+                        type="number" 
+                        name="size" 
+                        placeholder="Size" 
+                        onChange={handleChange} 
+                        value={formData.size} 
+                        className={inputCssStyles}
+                        required 
+                    />
+
+                    {/* PRODUCT PRICE */}
+                    <label htmlFor="productPrice" className={labelCssStyles}>
+                        Product Price
+                    </label>
+                    <input 
+                        type="number" 
+                        name="price" 
+                        placeholder="Price" 
+                        onChange={handleChange} 
+                        value={formData.price}
+                        className={inputCssStyles}
+                        required 
+                    />
+
+                    {/* STOCK QUANTITY */}
+                    <label htmlFor="stockQuantity" className={labelCssStyles}>
+                        Stock Quantity
+                    </label>
+                    <input
+                        type="number"
+                        name="stockQuantity"
+                        placeholder="Stock Quantity"
+                        onChange={handleChange}
+                        value={formData.stockQuantity}
+                        className={inputCssStyles}
+                        required 
+                    />
+
+                    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+                        Create
+                    </button>
+                    <button onClick={onClose} type="button" className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700">
+                        Cancel
+                    </button>
                 </form>
             </div>
         </div>
