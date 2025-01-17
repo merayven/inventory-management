f7beddead7b4db320afa167a627116d800bf83c8
Merayven <mpopiel@tuta.io>
2025-01-17 13:55:40 +0100

    Added margin to the buttons

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -123,7 +123,7 @@ const CreateProductModal = ({ isOpen, onClose, onCreate }: CreateProductModalPro
                         required 
                     />
 
-                    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+                    <button type="submit" className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
                         Create
                     </button>
                     <button onClick={onClose} type="button" className="ml-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700">
