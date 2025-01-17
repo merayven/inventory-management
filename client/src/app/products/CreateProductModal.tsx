524026aab746ed424ae07d9a9fb6e14f8d55324e
Merayven <mpopiel@tuta.io>
2025-01-17 13:33:30 +0100

    Create add product window with fields

---
+++ client/src/app/products/CreateProductModal.tsx
@@ -0,0 +1,74 @@
+import React, { ChangeEvent, FormEvent, useState } from 'react'
+import { v4 } from 'uuid';
+import Header from '@/app/(components)/Header';
+
+type ProductFormData = {
+    name: string;
+    brand: string;
+    size: number;
+    price: number;
+    stockQuantity: number;
+}
+
+type CreateProductModalProps = {
+    isOpen: boolean;
+    onClose: () => void;
+    onCreate: (formData: ProductFormData) => void;
+}
+
+const CreateProductModal = ({ isOpen, onClose, onCreate }: CreateProductModalProps) => {
+    const [formData, setFormData] = useState({
+        productId: v4(),
+        name: "",
+        brand: "",
+        size: 0,
+        price: 0,
+        stockQuantity: 0,
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        setFormData({
+            ...formData,
+            [name]:
+                name=== "brand" || name=== "price" || name=== "stockQuantity"
+                    ? parseFloat(value)
+                    : value,
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        onCreate(formData);
+        onClose();
+    }
+
+    if (!isOpen) return null;
+
+    const labelCssStyles = "block text-sm font-medium text-gray-700"
+    const inputCssStyles = "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md"
+
+    return (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
+            <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+                <Header name="Create New Product" />
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="productName" className={labelCssStyles}>
+                        Product Name
+                    </label>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        placeholder="Name" 
+                        onChange={handleChange} 
+                        value={formData.name} 
+                        className={inputCssStyles}
+                        required 
+                    />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default CreateProductModal
