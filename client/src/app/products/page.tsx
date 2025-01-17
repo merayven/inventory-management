2b1288b39b874361631d073cf7780ab018e41fee
Merayven <mpopiel@tuta.io>
2025-01-17 12:57:45 +0100

    Initialised page for /products

---
+++ client/src/app/products/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useGetProductsQuery } from "@/state/api";
+import { useState } from "react"
+
+const Products = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const { data: products, isLoading, isError } = useGetProductsQuery(searchTerm);
+
+    if (isLoading) {
+        return <div className="py-4">Loading...</div>
+    }
+
+    if (isError || !products) {
+        return (
+            <div className="text-center text-red-500 py-4">
+                Failed to fetch products
+            </div>
+        )
+    }
+
+    return (
+        <div className="mx-auto pb-5 w-full">
+
+        </div>
+    )
+}
+
+export default Products
