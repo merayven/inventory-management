621086e006f3a083492fcd07bf8a1b24862ec82e
Merayven <mpopiel@tuta.io>
2025-01-17 12:26:21 +0100

    Added getProducts api ednpoint with search param

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -55,12 +55,19 @@ export interface DashboardMetrics {
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
-    tagTypes: ["DashboardMetrics"],
+    tagTypes: ["DashboardMetrics", "Products"],
     endpoints: (build) => ({
         getDashboardMetrics: build.query<DashboardMetrics, void>({
             query: () => "/dashboard",
             providesTags: ["DashboardMetrics"]
-        })
+        }),
+        getProducts: build.query<Product[], string | void>({
+          query: (search) => ({
+            url: "/products",
+            params: search ? { search } : {}
+          }),
+          providesTags: ["Products"]
+      }),
     }),
 })
 
