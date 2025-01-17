937fd2119d769025a62deeba9aeab120a2a59ab9
Merayven <mpopiel@tuta.io>
2025-01-17 13:10:55 +0100

    Basic products page layout with searchbar and button for creating a new product

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useGetProductsQuery } from "@/state/api";
+import { PlusCircleIcon, SearchIcon } from "lucide-react";
 import { useState } from "react"
+import Header from "@/app/(components)/Header";
 
 const Products = () => {
     const [searchTerm, setSearchTerm] = useState("");
@@ -23,7 +25,38 @@ const Products = () => {
 
     return (
         <div className="mx-auto pb-5 w-full">
+            <div className="flex items-center border-2 border-gray-200 rounded">
+                <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+                <input className="w-full py-2 px-4 rounded bg-white" placeholder="Search product..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
 
+            <div className="flex justify-between items-center mb-6">
+                <Header name="Products" />
+                <button
+                    className="flex items-center bg-blue-500 hover:bg-blue-700 text-gray-200 font-bold py-2 px-4 rounded"
+                    onClick={() => setIsModalOpen(true)}
+                >
+                    <PlusCircleIcon className="w-5 h-5 mr-2 !text-gray-200" />
+                    Create Product
+                </button>
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg-grid-cols-3 gap-10 justify-between">
+                {isLoading ? (<div>Loading...</div>) : (
+                    products?.map((product) => (
+                        <div key={product.productId} className=" border shadow rounded-md p-4 max-w-full w-full mx-auto">
+                            <div className="flex flex-col items-center">
+                                img
+                                <h3 className="text-lg text-gray-900 font-semibold">{product.name}</h3>
+                                <p className="text-gray-800">${product.price.toFixed(2)}</p>
+                                <div className="text-sm text-gray-600 mt-1">
+                                    Stock: {product.stockQuantity}
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
     )
 }
